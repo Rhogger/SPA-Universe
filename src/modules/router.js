589478ad5ff760235161a5258995936d22d34a58
rhogger <rhoggerrv@gmail.com>
2024-03-19 00:35:14 -0300

    :sparkles: feat: Mudança de background ao trocar de páginas

--- src/modules/router.js
+++ src/modules/router.js
@@ -23,11 +23,37 @@ class Router {
 
     const route = this.routes[pathname] || this.routes['/error-404']
 
+    console.log(route);
+
+    this.changeBackground(route)
+
     fetch(route)
       .then(data => data.text())
       .then(html => {
         document.querySelector('main').innerHTML = html
       })
+  }
+
+  changeBackground(route) {
+    const body = document.querySelector('body')
+
+    console.log(body);
+
+    let url
+
+    if (route == 'src/pages/home.html' || route == 'src/pages/page-not-found.html') {
+      url = 'src/assets/images/mountains-universe-1.png'
+    } else if (route == 'src/pages/universe.html') {
+      url = 'src/assets/images/mountains-universe-2.png'
+    } else if (route == 'src/pages/exploration.html') {
+      url = 'src/assets/images/mountains-universe-3.png'
+    }
+
+    console.log(url);
+
+    body.style.backgroundImage = `url(${url})`
+
+    console.log(body.style);
   }
 }
 
